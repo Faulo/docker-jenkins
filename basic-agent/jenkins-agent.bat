50dfdb9a1172ad233f54bc0063c0a3bf9ae670d6
Daniel Schulz <faulolio@gmail.com>
2025-01-04 15:58:05 +0100

    fix: %

--- basic-agent/jenkins-agent.bat
+++ basic-agent/jenkins-agent.bat
@@ -1,2 +1,2 @@
-curl.exe !JENKINS_URL!jnlpJars/agent.jar -o agent.jar
-java !JENKINS_JAVA_OPTS! -jar agent.jar -url !JENKINS_URL! -secret !JENKINS_SECRET! -name "!JENKINS_AGENT_NAME!" -workDir "!JENKINS_AGENT_WORKDIR!"
+curl.exe %JENKINS_URL%jnlpJars/agent.jar -o agent.jar
+java %JENKINS_JAVA_OPTS% -jar agent.jar -url %JENKINS_URL% -secret %JENKINS_SECRET% -name "%JENKINS_AGENT_NAME%" -workDir "%JENKINS_AGENT_WORKDIR%"
