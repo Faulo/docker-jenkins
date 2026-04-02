0d558d1cdc7ad75d9f0696704bf698d4decdb679
Daniel Schulz <faulolio@gmail.com>
2026-04-02 21:04:47 +0200

    refactor: add .dockerfile

--- docker-build.bat
+++ docker-build.bat
@@ -2,13 +2,13 @@ setlocal
 cd %~dp0
 call load-env
 pushd basic-agent
-call docker build --isolation=hyperv . -f Dockerfile.%DOCKER_OS_TYPE% -t faulo/basic-agent:latest-%DOCKER_OS_TYPE%
+call docker build --isolation=hyperv . -f Dockerfile.%DOCKER_OS_TYPE%.dockerfile -t faulo/basic-agent:latest-%DOCKER_OS_TYPE%
 popd
 pushd ci-agent
-call docker build --isolation=hyperv . -f Dockerfile.%DOCKER_OS_TYPE% -t faulo/ci-agent:latest-%DOCKER_OS_TYPE%
+call docker build --isolation=hyperv . -f Dockerfile.%DOCKER_OS_TYPE%.dockerfile -t faulo/ci-agent:latest-%DOCKER_OS_TYPE%
 popd
 pushd unity-agent
-call docker build --isolation=hyperv . -f Dockerfile.%DOCKER_OS_TYPE% -t faulo/unity-agent:latest-%DOCKER_OS_TYPE%
+call docker build --isolation=hyperv . -f Dockerfile.%DOCKER_OS_TYPE%.dockerfile -t faulo/unity-agent:latest-%DOCKER_OS_TYPE%
 popd
 endlocal
 pause
